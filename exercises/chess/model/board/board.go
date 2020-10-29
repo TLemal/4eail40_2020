735bc58a91e4c5b8f3983fc13a10367400590841
TLemal <thomas.lemal@hotmail.com>
2020-10-29 12:06:05 +0100

    Implémentation de fonctions

--- exercises/chess/model/board/board.go
+++ exercises/chess/model/board/board.go
@@ -1,6 +1,8 @@
 package board
 
 import (
+	"fmt"
+
 	"github.com/jglouis/4eail40_2020/exercises/chess/model/coord"
 	"github.com/jglouis/4eail40_2020/exercises/chess/model/piece"
 )
@@ -9,24 +11,48 @@ import (
 type Classic [8][8]piece.Piece
 
 func (c *Classic) String() string {
-	panic("not implemented") // TODO: Implement
+	panic("Not implemented") //TODO : Implement
 }
 
 // PieceAt retrievves piece at give coordinates.
 // Returns nil if no piece was found.
 func (c *Classic) PieceAt(at coord.ChessCoordinates) piece.Piece {
-	panic("not implemented") // TODO: Implement
+	x, _ := at.Coord(0)
+	y, _ := at.Coord(1)
+	if x > 8 || y > 8 {
+		return nil
+	}
+	piece := c[x][y]
+	if piece != nil {
+		return piece
+	}
+	return nil
 }
 
 // MovePiece moves a piece from given coordinates to
 // given coordinates.
 // Returns an error if destination was occupied.
 func (c *Classic) MovePiece(from coord.ChessCoordinates, to coord.ChessCoordinates) error {
-	panic("not implemented") // TODO: Implement
+	if c.PieceAt(from) == nil {
+		return fmt.Errorf("No piece found at %v", from.String())
+	} else if c.PieceAt(to) != nil {
+		return fmt.Errorf("Destination occupied")
+	} else {
+		x, _ := from.Coord(0)
+		y, _ := from.Coord(1)
+		piece := c[x][y]
+		return c.PlacePieceAt(piece, to)
+	}
 }
 
 // PlacePieceAt places a given piece at given location.
 // Returns an error if destination was occupied.
 func (c *Classic) PlacePieceAt(p piece.Piece, at coord.ChessCoordinates) error {
-	panic("not implemented") // TODO: Implement
+	if c.PieceAt(at) == nil {
+		x, _ := at.Coord(0)
+		y, _ := at.Coord(1)
+		c[x][y] = p
+		return nil
+	}
+	return fmt.Errorf("Destination occupied")
 }
--- exercises/chess/model/coord/cartesian.go
+++ exercises/chess/model/coord/cartesian.go
@@ -1,4 +1,4 @@
-package coord
+package coord 
 
 import "fmt"
 
